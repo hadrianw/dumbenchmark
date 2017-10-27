3f0b1ad3974eb4b40f7f3d2dc7748fb01c3a334e
Hadrian Węgrzynowski <hadrianw@users.noreply.github.com>
2017-10-27 22:44:29 +0200

    Preliminary work on search.

--- search.c
+++ search.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <sqlite3.h>
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName)
+#define MATCH_QUERY "SELECT * FROM index WHERE index MATCH ?", 
+
+static int
+callback(void *NotUsed, int argc, char **argv, char **azColName)
 {
 	int i;
 	for(i = 0; i < argc; i++){
@@ -11,25 +14,38 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName)
 	return 0;
 }
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
+	int rc = -1;
 	sqlite3 *db;
 	char *err = 0;
-	int rc = -1;
+	sqlite3_stmt *match_stmt;
 	
 	if(sqlite3_open(argv[1], &db)) {
 		fprintf(stderr, "sqlite3_open failed: %s\n", sqlite3_errmsg(db));
 		goto out_close_db;
 	}
 	
-	if(sqlite3_exec(db, argv[2], callback, 0, &err) != SQLITE_OK) {
-		fprintf(stderr, "sqlite3_exec failed: %s\n", err);
-		sqlite3_free(zErrMsg);
+	if(sqlite3_prepare_v2(db, MATCH_QUERY, sizeof(MATCH_QUERY), &match_stmt, NULL) != SQLITE_OK) {
+		fprintf(stderr, "sqlite3_prepare_v2 failed: %s\n", sqlite3_errmsg(db));
 		goto out_close_db;
 	}
 	
-	rc = 0;
-	
+	if(sqlite3_bind_text(match_stmt, 1, argv[2], strlen(argv[2]), SQLITE_STATIC) != SQLITE_OK) {
+		fprintf(stderr, "sqlite3_bind_text failed: %s\n", sqlite3_errmsg(db));
+		goto out_finalize_stmt;
+	}
+
+	while((rc = sqlite3_step(insert_stmt) == SQLITE_ROW)) {
+		// show results
+	}
+	if(rc != 0) {
+		fprintf(stderr, "sqlite3_step failed: %s\n", sqlite3_errmsg(db));
+	}
+
+out_finalize_stmt:
+	sqlite3_finalize(match_stmt);
 out_close_db:
 	sqlite3_close(db);
 	return rc;
